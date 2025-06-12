902ef785c46254bc1185a0fdd9903667df6fd4ad
senthh <senthil.kumar@acceldata.io>
2025-06-12 16:13:52 +0530

    ODP-4498 - Changing jdk version to 11 in mongo-kafka version 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,7 +102,7 @@ tasks.withType<JavaCompile> {
     options.release.set(8)
 }
 
-val defaultJdkVersion = 17
+val defaultJdkVersion = 11
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(defaultJdkVersion))
