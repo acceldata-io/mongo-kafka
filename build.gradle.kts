451f182390a813fb0c64a6e33eb149190fca998d
dilrajsourabh <dilrajsourabh@gmail.com>
2026-05-25 13:50:58 +0530

    Changed version from '3.3.6.4-1' to '3.3.6.5-SNAPSHOT'

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "org.mongodb.kafka"
-version = "2.0.0.3.3.6.4-1"
+version = "2.0.0.3.3.6.5-SNAPSHOT"
 description = "The official MongoDB Apache Kafka Connect Connector."
 
 repositories {
