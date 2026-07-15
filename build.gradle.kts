09d10cf3b3f6cf095bfd70ffa6f6e0e3c2e8681f
dilrajsourabh <dilrajsourabh@gmail.com>
2026-07-15 16:50:00 +0530

    Update ODP versoin to 3.3.6.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 }
 
 group = "org.mongodb.kafka"
-version = "1.13.0.3.3.6.4-1"
+version = "1.13.0.3.3.6.5-SNAPSHOT"
 description = "The official MongoDB Apache Kafka Connect Connector."
 
 repositories {
